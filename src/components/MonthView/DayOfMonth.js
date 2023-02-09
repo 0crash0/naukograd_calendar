18ee514222d0bb6ca9ee54bd6066a8c0236bd7f7
0crash0 <0crash0@bk.ru>
2023-02-10 00:32:12 +0300

    added monthview : working on events

--- src/components/MonthView/DayOfMonth.js
+++ src/components/MonthView/DayOfMonth.js
@@ -1,17 +1,21 @@
 import moment from "moment";
 import * as events from "events";
 function DayOfMonth(props) {
-    console.log(props.events)
+
+
     return (
         <div className={"DayOfMonth" +(props.isToDay ? " ToDay" : "") +(props.isWeekend ? " WeekendDay" : "")+(props.isOtherMonth? " OtherMonth":"") + (props.isHeader? " DayOfWeek":"")}>
-            <h1 className={"Day"}>{props.day}</h1>
-            {!props.isHeader ? (
-                <div className="events">
-                    <div>{(    typeof props.events[0]  !== 'undefined'? props.events[0].title : "event1")}</div>
-                    <div>{(    typeof props.events[1]  !== 'undefined'? props.events[1].title : "event2")}</div>
-                    <div>{(    typeof props.events[2]  !== 'undefined'? props.events[2].title : "event3")}</div>
-                </div>
-            ) : ""
+            <div className={"divDay"}><h1 className={"Day"}>{props.day}</h1></div>
+            {!props.isHeader  &&
+                typeof props.events[0] !== 'undefined' &&
+                <ul className={"events"}>
+
+                    <button>{(    typeof props.events[0]  !== 'undefined'? props.events[0].title : "event1")}</button>
+                    <button>{(    typeof props.events[1]  !== 'undefined'? props.events[1].title : "event2")}</button>
+                    <button>{(    typeof props.events[2]  !== 'undefined'? props.events[2].title : "event3")}</button>
+                    {(props.events.length>2) &&  <div>more+</div>}
+                </ul>
+
             }
 
         </div>
--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -4,9 +4,9 @@ function ToolBar(props) {
     return(
         <div className="ToolBar">
             <div className="GO">
-                <div onClick={props.prevHandler} key="keyleft">&lt;</div>
+                <div className={"left"} onClick={props.prevHandler} key="keyleft">&lt;</div>
                 <div onClick={props.todayHandler} key="keytoday">today</div>
-                <div onClick={props.nextHandler}  key="keyright">&gt;</div>
+                <div className={"right"}  onClick={props.nextHandler}  key="keyright">&gt;</div>
             </div>
             <div className="ViewSet">
                 <div onClick={props.setViewWeek}  key="keyweek">week</div>
